68ec70bcbbb801f795c2ab959dbfd46533e5f3de
JanPack <78834700+JanPack@users.noreply.github.com>
2025-09-25 17:25:21 +0900

    Answer sheet for assignment 1 as an R file

---
+++ Assignment 1.R
@@ -0,0 +1,47 @@
+#Answer sheet for RIKEN-Karolinska workshop assignment 1
+#Jan Pack
+#25.9.2025
+
+#Task 1 - Literature
+#1.2a: Medical Relevant insights
+#This paper analyses the development of Oligodendrocyte precursor cells (OPC) in human
+#embryos at post-conception weeks 8-10. Oligodendrocytes play a vital role in the
+#proper function of the human brain, as they are responsible for forming the 
+#myelin sheath around myelinated axons. It is important to fully understand the
+#characteristics of OPCs as a deficiency can lead to developmental errors as well
+#as diseases later in life such as multiple sclerosis. In this paper, the authors
+#found a "wave" of OPC development that was known in mice but not in humans and 
+#indicates an earlier development of OPCs than previously assumed.
+
+#1.2b: genomic technologies:
+#The main technologies of this paper are single-cell RNA sequencing, single-cell 
+#ATAC-seq, and in-situ sequencing/hybridization. With this data they can inferments
+#of the developmental trajectory of OPCs and other cell types in the brain.
+
+#1.3a: extending the analysis
+#To extend the analysis, I would like to look at following three points:
+#A: How exactly is the chromatin remodeled prior to oligodendrogenesis?
+#B: How are the developmental "waves" triggered, and what determines their timing?
+#C: How are errors in this developmental stage related to diseases later in life?
+
+#1.3b: As the chromatin remodeling would occur previous to the timepoint of the 
+#data analysis of the paper, the responsible proteins are likely not expressed
+#anymore. To overcome this, maybe the protein activity could be plotted from mouse
+#data first, and see if there are any remodelers that are still expressed even after
+#the pre-oligodendrogenesis preparation is completed. Those proteins could then be 
+#looked for in the scRNA data of the human cells. A similar process where mouse 
+#data is consulted first could be used for elucidating the wave triggers.
+#As for disease causing errors, public databases such as UKbiobank could be consulted
+#to see if there is a correlation with mutations in the transcription factors 
+#detected in this study with disorders. 
+
+
+
+
+
+
+
+
+
+
+
